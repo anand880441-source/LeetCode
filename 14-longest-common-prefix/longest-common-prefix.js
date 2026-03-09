3587b7d52549b5124ecd74e50478184ef7c1d1e5
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-03-09 18:10:32 +0530

    Time: 1 ms (59.36%) | Memory: 55.2 MB (29.86%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (!strs || strs.length === 0) {
+        return "";
+    }
+    
+    strs.sort();
+    
+    const first = strs[0];
+    const last = strs[strs.length - 1];
+    let i = 0;
+    
+    while (i < first.length && i < last.length && first[i] === last[i]) {
+        i++;
+    }
+    
+    return first.slice(0, i);
+};
