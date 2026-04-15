2c24a7472b78a9c38fed31e0c3a6e0f3bb93cb86
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-04-15 10:12:44 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (98.59%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> m1;
-        unordered_map<char, char> m2;
+        vector<int> m1(128, -1); 
+        vector<int> m2(128, -1); 
 
-        for (int i = 0; i < s.length(); i++) {
-            char sc = s[i];
-            char tc = t[i];
+        for (int i = 0; i < s.size(); i++) {
+            char c1 = s[i];
+            char c2 = t[i];
 
-            if (m1.count(sc) && m1[sc] != tc){
-                return false;
-            };
+            if (m1[c1] != -1 && m1[c1] != c2) return false;
+            if (m2[c2] != -1 && m2[c2] != c1) return false;
 
-            if (m2.count(tc) && m2[tc] != sc) {
-                return false;
-            };
-
-            m1[sc] = tc;
-            m2[tc] = sc;
+            m1[c1] = c2;
+            m2[c2] = c1;
         }
+
         return true;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+//         unordered_map<char, char> m1;
+//         unordered_map<char, char> m2;
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char sc = s[i];
+//             char tc = t[i];
+
+//             if (m1.count(sc) && m1[sc] != tc){
+//                 return false;
+//             };
+
+//             if (m2.count(tc) && m2[tc] != sc) {
+//                 return false;
+//             };
+
+//             m1[sc] = tc;
+//             m2[tc] = sc;
+//         }
+//         return true;
+//     }
+// };
