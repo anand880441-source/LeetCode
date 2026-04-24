7888cab5aa33b48785d9dd990226ea6d5acf54eb
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-04-24 11:19:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (5.05%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string res = "";
+
+        for(int i = 0; i < s.size(); i++){
+            char c = s[i];
+    
+            if (c >= 'A' && c <= 'Z') {
+                res += (c + 32); 
+            } 
+            else if (c >= 'a' && c <= 'z') {
+                res += c;
+            } 
+            else if (c >= '0' && c <= '9') {
+                res += c;
+            }
+        }
+
+        string rev(res.rbegin(), res.rend());
+
+        return res == rev;
+    }
+};
