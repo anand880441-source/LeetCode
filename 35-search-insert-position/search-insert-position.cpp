f9ec99ad83271e48175949e5082650b75d090f50
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-05-09 14:32:14 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (98.32%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -3,18 +3,17 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int first = 0;
         int last = nums.size() - 1;
-        int expected = 0;
 
         while(first <= last){
-            if(nums[first] >= target){
-                return first;
-            }else if(nums[last] == target){
-                return last;
-            }else if(nums[last] < target){
-                return last + 1;
+            int mid = first + (last - first)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                first = mid + 1;
+            }else{
+                last = mid - 1;
             }
-            first++;
-            last--;
         }
         return first;
     }
