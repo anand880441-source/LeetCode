e2f2d519f8554f9416460779937e00ee57d4e62f
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-05-26 21:29:32 +0530

    Time: 1 ms (61.77%) | Memory: 13.9 MB (92.01%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> current;
+        
+        solve(candidates, target, 0, current, result);
+        
+        return result;
+    }
+    
+    void solve(vector<int>& candidates, int target, int start, 
+               vector<int>& current, vector<vector<int>>& result) {
+        
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }
+        
+        if (target < 0) {
+            return;
+        }
+        
+        for (int i = start; i < candidates.size(); i++) {
+            current.push_back(candidates[i]);
+            solve(candidates, target - candidates[i], i, current, result);
+            current.pop_back();
+        }
+    }
+};
