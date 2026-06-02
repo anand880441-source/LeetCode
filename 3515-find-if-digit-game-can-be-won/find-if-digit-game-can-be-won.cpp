13d441dfbee26e7551018878a7971eed29b51a5a
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-06-02 19:00:38 +0530

    Time: 0 ms (100.00%) | Memory: 28.6 MB (47.19%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int single_digit_sum = 0;
+        int double_digit_sum = 0;
+        
+        for (int num : nums) {
+            if (num < 10) {
+                single_digit_sum += num;
+            } else {
+                double_digit_sum += num;
+            }
+        }
+        
+        return single_digit_sum != double_digit_sum;
+    }
+};
