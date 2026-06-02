bf750358d8afecf29781c19d2518e88d20de521d
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-06-02 18:22:37 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (62.28%) - LeetSync

---
+++ 3778-transform-array-by-parity/transform-array-by-parity.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        vector<int> res;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i]%2 == 0){
+                res.insert(res.begin(), 0);
+            }
+            else{
+                res.push_back(1);
+            }
+        }
+
+        return res;
+
+    }
+};
