7cd3bebae537de0e60acc3cd8a3d2e8a4f2a5437
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-06-05 13:30:43 +0530

    Time: 3 ms (80.57%) | Memory: 13.5 MB (90.90%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res = "";
+
+        for(char ch: s){
+            if(!res.empty() && res.back() == ch){
+                res.pop_back();
+            }
+            else{
+                res.push_back(ch);
+            }
+        }
+
+        return res;
+    }
+};
