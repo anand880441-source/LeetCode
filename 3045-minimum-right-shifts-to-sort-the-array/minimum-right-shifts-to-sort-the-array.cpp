bfa7b170697f99f036aee1a164b41d4fda89cc57
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-06-08 20:11:50 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (91.57%) - LeetSync

--- 3045-minimum-right-shifts-to-sort-the-array/minimum-right-shifts-to-sort-the-array.cpp
+++ 3045-minimum-right-shifts-to-sort-the-array/minimum-right-shifts-to-sort-the-array.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int minimumRightShifts(vector<int>& nums) {
-         int ind=0;
-        int n=nums.size();
-        while(ind+1<n&&nums[ind]<nums[ind+1])ind++;
+        int ind = 0;
+        int n = nums.size();
+
+        while(ind + 1 < n && nums[ind] < nums[ind+1]) ind++;
         
-        if(ind==n-1)return 0;
+        if(ind == n-1) return 0;
         
         ind++;
-        int pivot=ind;
-       while(ind+1<n&&nums[ind]<nums[ind+1])ind++;
-        if(ind!=n-1)return -1;
-        if(nums[pivot]<nums[0]&&nums[n-1]<nums[0])
+
+        int pivot = ind;
+
+        while(ind + 1 < n && nums[ind] < nums[ind+1]) ind++;
+
+        if(ind != n-1) return -1;
+        
+        if(nums[pivot] < nums[0] && nums[n-1] < nums[0])
         {
             return n-pivot;
         }
