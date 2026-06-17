ec9c26c2da5bef8d2570ca5f6c9da6899ae1bdb2
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-06-17 09:50:05 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (80.64%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int search(std::vector<int>& nums, int target) {
+    int search(vector<int>& nums, int target) {
         int low = 0;
         int high = nums.size() - 1;
         
