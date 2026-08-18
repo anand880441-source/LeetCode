46ddea2bdd5bb4f2861ebc52c18320df3330b8f7
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-08-18 13:26:23 +0530

    Time: 16 ms (14.27%) | Memory: 19.5 MB (85.80%) - LeetSync

---
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int res = 0;
+        for(int i = 1; i<nums.size(); i++){
+            if(nums[i] <= nums[i-1]){
+                res = res + nums[i-1] - nums[i] + 1;
+                nums[i] = nums[i-1] + 1;
+            }
+        }
+        return res;
+    }
+};
