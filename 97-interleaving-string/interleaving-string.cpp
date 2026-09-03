49faa2f42f95442b000b3aba752b2873158b208f
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-09-03 19:23:11 +0530

    Time: 4 ms (52.36%) | Memory: 9.6 MB (45.71%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -4,33 +4,41 @@ public:
         int n = s1.length();
         int m = s2.length();
 
-        if(n + m != s3.length()){
+        if (n + m != s3.length()) {
             return false;
         }
 
-        if(s1 == "abababababababababababababababababababababababababababababababababababababababababababababababababbb" && s2 == "babababababababababababababababababababababababababababababababababababababababababababababababaaaba" && s3 == "abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababbb"){
-            return false;
-        }
+        vector<vector<int>> memo(n + 1, vector<int>(m + 1, -1));
+        return helper(s1, s2, s3, 0, 0, memo);
+    }
 
-       return helper(s1, s2, s3, 0, 0, 0);
-    };
-
-    bool helper(string &s1, string &s2, string &s3, int i, int j, int k){
-        if(i == s1.length() && j == s2.length() && k == s3.length()){
+    bool helper(string& s1, string& s2, string& s3, int i, int j,
+                vector<vector<int>>& memo) {
+        if (i == s1.length() && j == s2.length()) {
             return true;
         }
 
-        if(i<s1.length() && s1[i] == s3[k]){
-            if(helper(s1,s2,s3,i+1,j,k+1)){
+        if (memo[i][j] != -1) {
+            return memo[i][j] == 1;
+        }
+
+        int k = i + j;
+
+        if (i < s1.length() && s1[i] == s3[k]) {
+            if (helper(s1, s2, s3, i + 1, j, memo)) {
+                memo[i][j] = 1;
                 return true;
             }
         }
 
-        if(j<s2.length() && s2[j] == s3[k]){
-            if(helper(s1,s2,s3,i,j+1,k+1)){
+        if (j < s2.length() && s2[j] == s3[k]) {
+            if (helper(s1, s2, s3, i, j + 1, memo)) {
+                memo[i][j] = 1;
                 return true;
             }
         }
+
+        memo[i][j] = 0;
         return false;
     }
 };
