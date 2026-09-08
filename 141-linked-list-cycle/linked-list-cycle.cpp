288e2cbbc99b76c5914e2f9913ae3140df01ab8c
ANAND SUTHAR <anand.suthar.cg@gmail.com>
2026-09-08 10:17:06 +0530

    Time: 21 ms (5.02%) | Memory: 14.6 MB (6.40%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -6,20 +6,22 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
-
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
+        if(head == nullptr || head -> next == nullptr){
+            return false;
+        }
 
-        while(fast != nullptr && fast -> next != nullptr){
-            slow = slow -> next;
-            fast = fast -> next -> next;
-            if(slow == fast){
+        unordered_set<ListNode*> address;
+        ListNode* temp = head;
+
+        while(temp != nullptr){
+            if(address.count(temp)){
                 return true;
             }
+            address.insert(temp);
+            temp = temp -> next;
         }
 
         return false;
